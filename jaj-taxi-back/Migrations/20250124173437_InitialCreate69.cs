b3f040fac1f1e9c7b33df5487da89128d636bb9e
ishyjamz <ishtiakjamil@outlook.com>
2025-02-01 23:14:23 +0000

    wow

---
+++ jaj-taxi-back/Migrations/20250124173437_InitialCreate69.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace jaj_taxi_back.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate69 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
